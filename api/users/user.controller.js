b29a0347e1bc11305d5f8b2d93c3ae16d6d1e28c
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2024-03-11 11:55:07 +0500

    define CRUD API

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,26 +1,47 @@
-const {create} = require("./user.service");
+const {
+  create,
+  getUsers,
+  getUserById,
+  updateUsers,
+  deleteUsers,
+} = require("./user.service");
 const { hashSync, genSaltSync } = require("bcrypt");
 
-
-module.exports={
-    createUser: (req,res)=>{
-        const body = req.body;
-        console.log("Request body:", body);
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password,salt);
-        create(body,(err,results)=>{
-            if(err){
-                console.log(err)
-                return res.status(500).json({
-                    success:0,
-                    message:"Database Connection Error",
-                    error:err
-                })
-            }
-            return res.status(200).json({
-                success:1,
-                data:results
-            })
-        })
-    }
-}
+module.exports = {
+  createUser: (req, res) => {
+    const body = req.body;
+    console.log("Request body:", body);
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database Connection Error",
+          error: err,
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        console.log(err);1
+        return res.status(500).json({
+          success: 0,
+          message: "Database Connection Error",
+          error: err,
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+};
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,23 +1,77 @@
 const pool = require("../../config/database");
 
-module.exports={
-    create: (data, callback)=>{
-        pool.query(
-            `Insert into users (fname,lname,gender,email,password,phone) values(?,?,?,?,?,?)`,
-            [
-                data.fname,
-                data.lname,
-                data.gender,
-                data.email,
-                data.password,
-                data.phone
-            ],
-            (error,results,fields)=>{
-                if(error){
-                    return callback(error);
-                }
-                return  callback(null,results);
-            }
-        )
-    }
-}
+module.exports = {
+  create: (data, callback) => {
+    pool.query(
+      `Insert into users (fname,lname,gender,email,password,phone) values(?,?,?,?,?,?)`,
+      [
+        data.fname,
+        data.lname,
+        data.gender,
+        data.email,
+        data.password,
+        data.phone,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+  getUsers: (callback) => {
+    pool.query(`Select * From users`, [], (error, results, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+  getUserById: (id, callback) => {
+    pool.query(
+      `Select * From users where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results[0]);
+      }
+    );
+  },
+  updateUsers: (data, callback) => {
+    pool.query(
+      `Update users set fname=?,lname=?,gender=?,email=?,password=?,phone=? where id = ?`,
+      [
+        data.fname,
+        data.lname,
+        data.gender,
+        data.email,
+        data.password,
+        data.phone,
+        data.id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results[0]);
+      }
+    );
+  },
+  deleteUsers: (data, callback) => {
+    pool.query(
+      `Delete From users where id = ?`,
+      [
+        data.id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results[0]);
+      }
+    );
+  },
+};
