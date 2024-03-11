721e104797b3c2c90c9be73ab9eb9dcfb44a0a49
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2024-03-11 12:57:41 +0500

    CRUD API

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -31,16 +31,58 @@ module.exports = {
   getUsers: (req, res) => {
     getUsers((err, results) => {
       if (err) {
-        console.log(err);1
-        return res.status(500).json({
-          success: 0,
-          message: "Database Connection Error",
-          error: err,
-        });
+        return err;
       }
       return res.status(200).json({
         success: 1,
         data: results,
+      });
+    });
+  },
+  getUserById: (req, res) => {
+    const id = req.params.id;
+    getUserById(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if(!results){
+        return res.json({
+            success:0,
+            message:"Record Not Found"
+        })
+      }
+      return res.json({
+        success:1,
+        data: results
+      })
+    });
+  },
+  updateUsers: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    updateUsers(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.status(200).json({
+        success: 1,
+        data: "updated successfully",
+      });
+    });
+  },
+  deleteUsers: (req, res) => {
+    const body = req.body;
+    deleteUsers(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.status(200).json({
+        success: 1,
+        data: "Deleted successfully",
       });
     });
   },
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,7 +1,21 @@
-const {createUser} =  require("./user.controller");
+const {
+  createUser,
+  deleteUsers,
+  getUserById,
+  updateUsers,
+  getUsers
+} = require("./user.controller");
 
-const router  =  require("express").Router();
+const router = require("express").Router();
 
-router.post("/",createUser);
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.patch("/", updateUsers);
+router.delete("/", deleteUsers);
+
+
+
+
 
 module.exports = router;
